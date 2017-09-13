fb0e9b31aaa03af9e403fb8814a7a3ddfc13405f
stevendev <stevendotpulido@gmail.com>
2017-09-13 14:34:54 -0700

    updated css in footer

--- web/app/themes/webdesignsandiego/lib/setup.php
+++ web/app/themes/webdesignsandiego/lib/setup.php
@@ -102,9 +102,7 @@ function assets() {
   if (is_single() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
   }
-
   wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
   wp_enqueue_script('sage/smoothscroll', get_template_directory_uri() . '/assets/scripts/smoothscroll.min.js', ['jquery'], null, true);
-
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
