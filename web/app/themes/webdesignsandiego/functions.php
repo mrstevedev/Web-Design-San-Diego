b049c1e4a741240a37587bd298e4d88e1e6da15f
stevendev <stevendotpulido@gmail.com>
2017-09-14 16:57:42 -0700

    amp

--- web/app/themes/webdesignsandiego/functions.php
+++ web/app/themes/webdesignsandiego/functions.php
@@ -46,3 +46,4 @@ function twentyten_remove_recent_comments_style() {
   remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );  
 }  
 add_action( 'widgets_init', 'twentyten_remove_recent_comments_style' );
+
