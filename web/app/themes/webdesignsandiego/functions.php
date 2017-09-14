5930f51259774e5382c7bc6e3dbaf061e3d49c91
stevendev <stevendotpulido@gmail.com>
2017-09-14 15:07:49 -0700

    updated wordpress to remove emoji scripts and styles

--- web/app/themes/webdesignsandiego/functions.php
+++ web/app/themes/webdesignsandiego/functions.php
@@ -32,3 +32,10 @@ function cc_mime_types($mimes=array()) {
   return $mimes;
 }
 add_filter('upload_mimes', 'cc_mime_types', 1, 1);
+
+// REMOVE WP EMOJI
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
