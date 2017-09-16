d564509e1c9f9dcc44c5cc73cd39408cfed00b75
stevendev <stevendotpulido@gmail.com>
2017-09-15 18:24:05 -0700

    amp-form

--- web/app/themes/webdesignsandiego/home.php
+++ web/app/themes/webdesignsandiego/home.php
@@ -10,61 +10,78 @@
     <div class="request-a-quote">
       <h3>Request a Quote</h3>
     </div>
-  <form method="post"
-    action-xhr="https://webdesignsandiego.stevenpulido.com"
-    target="_top">
-    <fieldset>
+    <form method="post"
+    class="p2"
+    action-xhr="/components/amp-form/submit-form-input-text-xhr"
+    target="_top"
+    custom-validation-reporting="show-all-on-submit">
+    <div class="ampstart-input inline-block relative m0 p0 mb3">
       <div class="amp-field-group col-lg-3">
-      <label class="amp-label-group">
-        <span class="amp-label">Name:</span>
         <input type="text"
+          class="block border-none p0 m0 amp-control"
+          id="show-all-on-submit-name"
           name="name"
-          class="amp-control"
-          required>
-      </label>
+          placeholder="Enter your Name..."
+          required
+          pattern="\w+\s\w+">
+      <span visible-when-invalid="valueMissing"
+        validation-for="show-all-on-submit-name"></span>
+      <span visible-when-invalid="patternMismatch"
+        validation-for="show-all-on-submit-name">
+        Please enter your first and last name separated by a space (e.g. Jane Miller)
+      </span>
       </div>
       <div class="amp-field-group col-lg-3">
-      <label class="amp-label-group">
-        <span class="amp-label">Email:</span>
-        <input type="email"
-          name="email"
-          class="amp-control"
-          required>
-      </label>
-      </div>
-      <div class="amp-field-group col-lg-3">
-      <label class="amp-label-group">
-        <span class="amp-label">Phone:</span>
-        <input type="text"
-          name="name"
-          class="amp-control"
-          required>
-      </label>
-      </div>
-      <div class="amp-field-group col-lg-3">
-      <label class="amp-label-group">
-        <span class="amp-label">Tell us about your project:</span>
-        <input type="text"
-          name="name"
-          class="amp-control"
-          required>
-      </label>
-      </div>
-      
-    </fieldset>
-    <div>
-    <input type="submit"
-        value="Request a Quote"
-        class="amp-submit-control">
+      <input type="email"
+        class="block border-none p0 m0 amp-control"
+        id="show-all-on-submit-email"
+        name="email"
+        placeholder="Enter your Email..."
+        required>
+      <span visible-when-invalid="valueMissing"
+        validation-for="show-all-on-submit-email"></span>
+      <span visible-when-invalid="typeMismatch"
+        validation-for="show-all-on-submit-email"></span>
     </div>
+    <div class="amp-field-group col-lg-3">
+      <input type="tel"
+        class="block border-none p0 m0 amp-control"
+        id="show-all-on-submit-tel"
+        name="tel"
+        placeholder="Enter your Phone..."
+        required>
+      <span visible-when-invalid="valueMissing"
+        validation-for="show-all-on-submit-tel"></span>
+      <span visible-when-invalid="typeMismatch"
+        validation-for="show-all-on-submit-tel"></span>
+    </div>
+    <div class="amp-field-group col-lg-3">
+      <input type="text"
+        class="block border-none p0 m0 amp-control"
+        id="show-all-on-submit-message"
+        name="email"
+        placeholder="Tell us about your project"
+        required>
+      <span visible-when-invalid="valueMissing"
+        validation-for="show-all-on-submit-message"></span>
+      <span visible-when-invalid="typeMismatch"
+        validation-for="show-all-on-submit-message"></span>
+    </div>
+    </div>
+    <input type="submit"
+      value="Request a Quote"
+      class="ampstart-btn caps amp-submit-control">
     <div submit-success>
       <template type="amp-mustache">
-        Subscription successful!
+        Success! Thanks {{name}} for trying the
+        <code>amp-form</code> demo! Try to insert the word "error" as a name input in the form to see how
+        <code>amp-form</code> handles errors.
       </template>
     </div>
     <div submit-error>
       <template type="amp-mustache">
-        Subscription failed!
+        Error! Thanks {{name}} for trying the
+        <code>amp-form</code> demo with an error response.
       </template>
     </div>
   </form>
