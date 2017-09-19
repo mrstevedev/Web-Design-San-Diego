dc590ee298f48c18ad840a99ada066cc29cc30d1
stevendev <stevendotpulido@gmail.com>
2017-09-18 18:04:33 -0700

    amp

--- web/app/themes/webdesignsandiego/post.php
+++ web/app/themes/webdesignsandiego/post.php
@@ -1,3 +1,4 @@
+<?php /*Template Name: post Template */ ?>
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
