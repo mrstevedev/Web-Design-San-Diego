eaf137c9c182a207c973ab35226fbc5de5db83ce
stevendev <stevendotpulido@gmail.com>
2017-10-02 20:33:17 -0700

    amp

--- web/app/themes/webdesignsandiego/lib/setup.php
+++ web/app/themes/webdesignsandiego/lib/setup.php
@@ -103,9 +103,25 @@ function assets() {
     wp_enqueue_script('comment-reply');
   }
 
-  \wp_deregister_script('jquery');
+  wp_deregister_script('jquery');
   // wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
   // wp_enqueue_script('sage/smoothscroll', get_template_directory_uri() . '/assets/scripts/smoothscroll.min.js', ['jquery'], null, true);
 
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
+
+add_action( 'init', function() {
+  
+      // Remove the REST API endpoint.
+      //remove_action('rest_api_init', 'wp_oembed_register_route');
+  
+      // Turn off oEmbed auto discovery.
+      // Don't filter oEmbed results.
+      //remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);
+  
+      // Remove oEmbed discovery links.
+      //remove_action('wp_head', 'wp_oembed_add_discovery_links');
+  
+      // Remove oEmbed-specific JavaScript from the front-end and back-end.
+      remove_action('wp_head', 'wp_oembed_add_host_js');
+  }, PHP_INT_MAX - 1 );
