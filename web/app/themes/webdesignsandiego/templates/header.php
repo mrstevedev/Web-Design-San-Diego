d0ad3bf9ab3f493d0494144d66c47e1a278a1fc8
stevendev <stevendotpulido@gmail.com>
2017-10-04 20:28:09 -0700

    mobile styles updated. no hamburger menu

--- web/app/themes/webdesignsandiego/templates/header.php
+++ web/app/themes/webdesignsandiego/templates/header.php
@@ -1,10 +1,10 @@
 <header class="banner fixed-top">
   <div class="container-fluid">   
     <nav class="navbar nav-primary navbar-expand-md navbar-light" data-theme="white" aria-label="Primary">
-        <a class="brand logo col-2 col-sm-2 col-md-2 col-lg-1" href="<?= esc_url(home_url('/')); ?>">
+        <a class="brand logo col-3 col-sm-2 col-md-2 col-lg-1" href="<?= esc_url(home_url('/')); ?>">
           <amp-img src="<?php bloginfo('url')?>/app/uploads/2017/09/logo.svg" srcset="<?php bloginfo('url')?>/app/uploads/2017/09/logo.svg 62w" alt="Welcome" width="92" height="70" layout="responsive"></amp-img>
         </a>          
-    <div class="form-inline col-7 col-md-3">
+    <div class="form-inline col-9 col-md-3">
     <div class="input-group">
       <div class="contact-container email-info-container">
         <a href="mailto:info@webdesignsandiego.com">info@webdesignsandiego.com</a>
@@ -17,9 +17,9 @@
       </div>
     </div>
 </div>
-<button id="navbar-toggler" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+<!-- <button id="navbar-toggler" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 <span class="navbar-toggler-icon"></span>
-</button>
+</button> -->
       <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
         <?php
         if (has_nav_menu('primary_navigation')) :
